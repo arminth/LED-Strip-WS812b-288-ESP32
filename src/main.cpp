ba0e64b592eb69a2bf7042dff6186da8bf7b703f
arminth <armin@a-thinnes.de>
2021-07-22 15:49:06 +0200

    co

--- src/main.cpp
+++ src/main.cpp
@@ -20,7 +20,9 @@
 Preferences preferences;
 #define FASTLED_INTERNAL        // Suppress build banner
 #include <FastLED.h>
-
+#include <WiFi.h>
+#include "soc/soc.h"
+#include "soc/rtc_cntl_reg.h"
 
 #define NUM_LEDS    52      // FastLED definitions
 #define LED_PIN     5
@@ -34,9 +36,15 @@ int g_PowerLimit = 4500;         // 900mW Power Limit
 int effect = 0;
 int touch = 0;
 const int treshold = 150; // If summ of 4 touchRead() in a row is below treshold, we have a touch! 
+const char* ssid     = "Nase2";
+const char* password = "Andrea_1234567";
+String greenstate = "off";// state of green LED
+String redstate = "off";// state of red LED
+String offstate = "off";// state of red LED
+WiFiServer server(80);// Set web server port number to 80
 
-
-
+String header; // Variable to store the HTTP request
+boolean modi = false;
 #include "comet.h"
 #include "marquee.h"
 #include "twinkle.h"
@@ -47,7 +55,24 @@ const int treshold = 150; // If summ of 4 touchRead() in a row is below treshold
 
 void setup() 
 
-{ 
+{ WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); //disable brownout detector
+  Serial.begin(115200);
+  while (!Serial) { }
+  Serial.println("ESP32 Startup");
+ WiFi.begin(ssid, password);
+Serial.print("Connecting to ");
+Serial.println(ssid);
+while (WiFi.status() != WL_CONNECTED) {
+  delay(500);
+  Serial.print(".");
+} 
+Serial.println("");
+Serial.println("WiFi connected.");
+Serial.println("IP address: ");
+Serial.println(WiFi.localIP());// this will display the Ip address of the Pi which should be entered into your browser
+ server.begin(); 
+  
+  
   preferences.begin("Lampe", false);
   effect = preferences.getInt("effect", 0);
   preferences.end();
@@ -55,14 +80,7 @@ void setup()
   pinMode(LED_PIN, OUTPUT);
  
 
-  Serial.begin(115200);
-  while (!Serial) { }
-  Serial.println("ESP32 Startup");
-  Serial.println("Effekt: Feuer");
-  
-  preferences.begin("Lampe", false);
-  effect = preferences.getInt("effect", 0);
-  preferences.end();
+ 
   FastLED.addLeds<WS2812B, LED_PIN, GRB>(g_LEDs, NUM_LEDS);               // Add our LED strip to the FastLED library
   
   FastLED.setBrightness(g_Brightness);
@@ -94,21 +112,132 @@ void loop()
 while (true)
   
   {
-  
-    if (touchRead(TOUCH_PIN)+touchRead(TOUCH_PIN)+touchRead(TOUCH_PIN)+touchRead(TOUCH_PIN)<treshold)
-    {delay (200);
-      if (touchRead(TOUCH_PIN)+touchRead(TOUCH_PIN)+touchRead(TOUCH_PIN)+touchRead(TOUCH_PIN)<treshold) //safe
-      {
-    effect ++;
+      WiFiClient client = server.available();   // Listen for incoming clients
+  if (client) {                             // If a new client connects,
+    String currentLine = "";                // make a String to hold incoming data from the client
+    while (client.connected()) {            // loop while the client's connected
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
+        header += c;
+        if (c == '\n') {                    // if the byte is a newline character
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println("Connection: close");
+            client.println();
+            
+            // turns the GPIOs on and off
+            if (header.indexOf("GET /green/on") >= 0) {
+              Serial.println("green on");
+              greenstate = "on";
+              effect ++;
+              modi = true;
+            } else if (header.indexOf("GET /green/off") >= 0) {
+              Serial.println("green off");
+              greenstate = "off";
+              effect ++;
+              modi = true;
+            } else if (header.indexOf("GET /red/on") >= 0) {
+              Serial.println("red on");
+              redstate = "on";
+              effect --;
+              modi = true;
+            } else if (header.indexOf("GET /red/off") >= 0) {
+              Serial.println("red off");
+              redstate = "off";
+              effect --;
+              modi = true;
+            } else if (header.indexOf("GET /off/on") >= 0) {
+              Serial.println("lamp off");
+              offstate = "on";
+              effect = 12;
+              modi = true;
+            }
+            else if (header.indexOf("GET /off/off") >= 0) {
+              Serial.println("lamp off");
+              offstate = "off";
+              effect = 12;
+              modi = true;
+            }
+            // Display the HTML web page
+            client.println("<!DOCTYPE html><html>");
+            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+            client.println("<link rel=\"icon\" href=\"data:,\">");
+            // CSS to style the on/off buttons 
+            // Feel free to change the background-color and font-size attributes to fit your preferences
+            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
+            client.println(".button { background-color: #195B6A; border: none; color: white; padding: 16px 40px;");
+            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
+            client.println(".button2 {background-color: #77878A;}</style></head>");
+            
+            // Web Page Heading
+            client.println("<body><h1>ESP32 Atomic Firelamp Web Server</h1>");
+            
+            // Display current state, and ON/OFF buttons for effect ++  
+            //client.println("<p>green - State " + greenstate + "</p>");
+            // If the green LED is off, it displays the ON button       
+            if (greenstate == "off") {
+              client.println("<p><a href=\"/green/on\"><button class=\"button\">Effect ++</button></a></p>");
+            } else {
+              client.println("<p><a href=\"/green/off\"><button class=\"button button2\">Effect ++</button></a></p>");
+            } 
+               
+            // Display current state, and ON/OFF buttons for effect -- 
+            //client.println("<p>red - State " + redstate + "</p>");
+            // If the red LED is off, it displays the ON button       
+            if (redstate == "off") {
+              client.println("<p><a href=\"/red/on\"><button class=\"button\"> Effect - - </button></a></p>");
+            } else {
+              client.println("<p><a href=\"/red/off\"><button class=\"button button2\"> Effect - - </button></a></p>");
+            }
+            client.println("</body></html>");
+            // Display current state, and ON/OFF buttons for Lamp Off 
+            //client.println("<p>red - State " + redstate + "</p>");
+            // If the red LED is off, it displays the ON button       
+            if (offstate == "off") {
+              client.println("<p><a href=\"/off/on\"><button class=\"button\">Lamp off</button></a></p>");
+            } else {
+              client.println("<p><a href=\"/off/off\"><button class=\"button button2\">Lamp off</button></a></p>");
+            }
+            client.println("</body></html>");
+            
+            // The HTTP response ends with another blank line
+            client.println();
+            // Break out of the while loop
+            break;
+          } else { // if you got a newline, then clear currentLine
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+      }
+    }
+    // Clear the header variable
+    header = "";
+    // Close the connection
+    client.stop();
+    Serial.println("Client disconnected.");
+    Serial.println("");
+  }
+
+
+ if (modi) {
     preferences.begin("Lampe", false);
     preferences.putInt("effect", effect);
     preferences.end();
     Serial.println("Gespeichert. Weiter! ");
     delay(500);
-      }
-      else { Serial.println("nix"); delay(1000);}
+    modi=false;
+    }
+    
         
-    };
+    
      
     
  
@@ -228,8 +357,8 @@ switch (effect) {
   
     
     
-  }
+  
 }
-
+}
 
 
