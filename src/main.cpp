ce044e7bbacb3b6bf04d2f2d00a3e56c67c994d2
arminth <armin@a-thinnes.de>
2021-07-25 10:31:26 +0200

    update

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,14 @@
 //
 // NightDriver - (c) 2018 Dave Plummer.  All Rights Reserved.
 //
-// File:        LED Episode 08
-//
+// Original File:        LED Episode 08
+// File:                 Atomic WiFi Firelamp
 // Description:
 //
-//   Draws sample effects on a an addressable strip using FastLED
-//
+//   Draws  effects on addressable strips using FastLED
+//   Provides WiFi access to controll the effects both in AP Mode and connected WiFi mode
+//   for the Atomic Fire Lamp
+//   https://www.youtube.com/watch?v=_wCOCI18nAk
 // History:     Sep-15-2020     davepl      Created
 //              Oct-05-2020     davepl      Revised for Episode 07
 //              Oct-11-2020     davepl      Revised for Episode 08
@@ -15,6 +17,7 @@
 //              06-06-2021      arminth     used and changed for own LED-Strip creation
 //                                          added effects pacifica and pride2015 from Mark Kriegsman                
 //              06-22-2021      arminth     added WiFi
+//              06-24-2021      arminth     included WiFiManager
 //---------------------------------------------------------------------------
 
 #include <Arduino.h>            // Arduino Framework
@@ -23,8 +26,8 @@ Preferences preferences;
 #define FASTLED_INTERNAL        // Suppress build banner
 #include <FastLED.h>
 #include <WiFi.h>
-
-
+#include <WiFiManager.h> 
+WiFiManager wm;
 #define NUM_LEDS    52      // FastLED definitions, 52 LED's per arm
 #define LED_PIN     5
 
@@ -37,14 +40,10 @@ int g_PowerLimit = 4500;         // 4500mW Power Limit
 int effect = 0;   // effect number
 int versuche = 0; // number of trys to connenct to WiFi
 
-const char* ssid     = "Nase2";
-const char* ssid2     = "Nase";
-const char* ssid3     = "Nase3";
-const char* ssid4     = "Nase4";
-const char* password = "Andrea_1234567";
+
 const char* softap_ssid     = "Firelamp";
 const char* softap_password = "12345678";
-
+String modes[13] = {"Fire","Balls","Comet","Twinkle","Marquee","Marquee Mirrored","Pacifica","Pride 2015", "Lamp full","Lamp less bright","Lamp medium","Lamp dimm","Lamp OFF"};
 String ssid_connected = softap_ssid; //which SSID are we connected
 
 
@@ -64,12 +63,13 @@ boolean modi = false; //configuration modified?
 #include "bounce.h"
 #include "pacifica.h"
 #include "pride2015.h"
+//start fire and bouncingballs
 ClassicFireEffect fire(NUM_LEDS, 40, 80, 50, 4, true, false); 
 BouncingBallEffect balls(NUM_LEDS, 5, 0, false, 4.0); // bouncing Balls
 
-void effect_on()
+void effect_on()  // update enabled selected effect
  {
-   switch (effect) { //enable actual effect
+   switch (effect) { 
  
   case 0:
     FastLED.clear();
@@ -113,15 +113,15 @@ void effect_on()
     
     case 9:
     fill_solid(g_LEDs,NUM_LEDS, CRGB::White);
-    FastLED.show(int(g_Brightness*0.75));
+    FastLED.show(int(g_Brightness*0.4));
     break;
     case 10:
     fill_solid(g_LEDs,NUM_LEDS, CRGB::White);
-    FastLED.show(int(g_Brightness*0.5));
+    FastLED.show(int(g_Brightness*0.25));
     break;
     case 11:
     fill_solid(g_LEDs,NUM_LEDS, CRGB::White);
-    FastLED.show(int(g_Brightness*0.25));
+    FastLED.show(int(g_Brightness*0.1));
     break;
     case 12:
     fill_solid(g_LEDs,NUM_LEDS, CRGB::Black);
@@ -132,6 +132,7 @@ void effect_on()
 
   } // of switch
  };
+
 void setup() 
 
 { pinMode(LED_BUILTIN, OUTPUT);
@@ -149,94 +150,23 @@ void setup()
   Serial.begin(115200);
   while (!Serial) { }
   Serial.println("ESP32 Startup");
- WiFi.begin(ssid, password);
- ssid_connected = ssid;
-Serial.print("Connecting to ");
-Serial.println(ssid);
-while ((WiFi.status() != WL_CONNECTED) and (versuche<10)) {
-  fill_solid(g_LEDs,NUM_LEDS, CRGB::Green);
-    FastLED.show(int(g_Brightness*0.75));
-  delay(250);
-  fill_solid(g_LEDs,NUM_LEDS, CRGB::Black);
-    FastLED.show(int(g_Brightness*0.75));
-    delay(250);
-  Serial.print(".");
-  versuche ++;
  
-} 
-if (versuche>=10)
-  { WiFi.disconnect(); 
-   versuche = 0;
-   WiFi.begin(ssid2, password);
-   ssid_connected = ssid2;
-   Serial.print("Connecting to ");
-   Serial.println(ssid2);
-   
-   while ((WiFi.status() != WL_CONNECTED) and (versuche<10)) {
-   fill_solid(g_LEDs,NUM_LEDS, CRGB::Yellow);
+fill_solid(g_LEDs,NUM_LEDS, CRGB::Blue); // indicate lamp start in green
     FastLED.show(int(g_Brightness*0.75));
-  delay(250);
-  fill_solid(g_LEDs,NUM_LEDS, CRGB::Black);
-    FastLED.show(int(g_Brightness*0.75));
-    delay(250);
-    Serial.print(".");
-    versuche ++;
-     } 
-    if (versuche>=10)
-  { WiFi.disconnect(); 
-   versuche = 0;
-   WiFi.begin(ssid3, password);
-   ssid_connected = ssid3;
-   Serial.print("Connecting to ");
-   Serial.println(ssid3);
-   while ((WiFi.status() != WL_CONNECTED) and (versuche<10)) {
-    fill_solid(g_LEDs,NUM_LEDS, CRGB::Orange);
-    FastLED.show(int(g_Brightness*0.75));
-  delay(250);
-  fill_solid(g_LEDs,NUM_LEDS, CRGB::Black);
-    FastLED.show(int(g_Brightness*0.75));
-    delay(250);
-    Serial.print(".");
-    versuche ++;
-   } 
-   if (versuche>=10)
-  { WiFi.disconnect(); 
-   versuche = 0;
-   WiFi.begin(ssid4, password);
-   ssid_connected = ssid4;
-   Serial.print("Connecting to ");
-   Serial.println(ssid4);
-   while ((WiFi.status() != WL_CONNECTED) and (versuche<10)) {
-    fill_solid(g_LEDs,NUM_LEDS, CRGB::Red);
-    FastLED.show(int(g_Brightness*0.75));
-  delay(250);
-  fill_solid(g_LEDs,NUM_LEDS, CRGB::Black);
-    FastLED.show(int(g_Brightness*0.75));
-    delay(250);
-    Serial.print(".");
-    versuche ++;
-   }
-  }
-  } 
-  }
-
-if (versuche <10) {Serial.println(""); // we have WiFi Access
-Serial.println("WiFi connected.");
-Serial.println("IP address: ");
-Serial.println(WiFi.localIP());// this will display the Ip address of the ESP which should be entered into your browser
 
 
- server.begin(); }
-  else { // We need to open AP
-    
-    WiFi.softAP(softap_ssid, softap_password);
+wm.setConnectTimeout(20); //First try to connect to known networks for 20 seconds, then switch to AP portal
+wm.setConfigPortalTimeout(60); //After 60 seconds of AP portal open and no activity, exit portal to enable local AP operation
+
+if (!(wm.autoConnect("Firelamp_AP_config", "12345678"))) { //no known network found, no input from  or captive AP-Portal was exited without selection of an SSID to connect to
+  WiFi.softAP(softap_ssid, softap_password); //we need to open our own AP for the lamp
     Serial.println("");
     ssid_connected = softap_ssid;
 Serial.println("AP WiFi provided.");
 Serial.println("IP address: ");
-Serial.println(WiFi.softAPIP());// this will display the Ip address of the ESP in SoftAP-Mode
+Serial.println(WiFi.softAPIP());// this will display the Ip address of the ESP in SoftAP-Mode (default: 192.168.4.1)
 
-for (int i=0; i<5; i++){
+for (int i=0; i<5; i++){ // Lamp is started in AP-Mode - 5 blue blinks
   fill_solid(g_LEDs,NUM_LEDS, CRGB::Blue);
     FastLED.show(int(g_Brightness*0.75));
   delay(250);
@@ -244,17 +174,27 @@ for (int i=0; i<5; i++){
     FastLED.show(int(g_Brightness*0.75));
     delay(250);}
 
+ }
+ else {ssid_connected = WiFi.SSID(); //we are connected to WiFi! 5 green blinks!
+ for (int i=0; i<5; i++){
+  fill_solid(g_LEDs,NUM_LEDS, CRGB::Green);
+    FastLED.show(int(g_Brightness*0.75));
+  delay(250);
+  fill_solid(g_LEDs,NUM_LEDS, CRGB::Black);
+    FastLED.show(int(g_Brightness*0.75));
+    delay(250);}
+    }  
 
+
+ server.begin(); // start webserver vor lamp
     
- server.begin();
-    };
   
 
   // read last Effect from the config-flash
   preferences.begin("Lampe", false);
   effect = preferences.getInt("effect", 0);
   preferences.end();
-  
+  Effekt = modes[effect];
 }
 
 
@@ -262,8 +202,6 @@ for (int i=0; i<5; i++){
 
 void loop() 
 {
-  
-
 
 
 while (true)
@@ -299,56 +237,47 @@ while (true)
             // Switches the effects
             if (header.indexOf("GET /fire/on") >= 0) {
               effect=0;
-              Effekt = "Fire";
               modi = true;
             } else if (header.indexOf("GET /balls/on") >= 0) {
               effect = 1;
-              Effekt = "Balls";
               modi = true;
             } else if (header.indexOf("GET /comet/on") >= 0) {
               effect =2;
-              Effekt = "Comet"; 
               modi = true;
             } else if (header.indexOf("GET /twinkle/on") >= 0) {
               effect =3;
-              Effekt = "Twinkle"; 
               modi = true;
             } else if (header.indexOf("GET /marquee/on") >= 0) {
               effect = 4;
-              Effekt = "Marquee"; 
               modi = true;
             }else if (header.indexOf("GET /marquee_m/on") >= 0) {
               effect = 5;
-              Effekt = "Marquee mirrored"; 
               modi = true;
             }else if (header.indexOf("GET /pacifica/on") >= 0) {
               effect = 6;
-              Effekt = "Pacifica"; 
               modi = true;
             }else if (header.indexOf("GET /pride2015/on") >= 0) {
               effect = 7;
-              Effekt = "Pride2015"; 
               modi = true;
             }else if (header.indexOf("GET /lamp100/on") >= 0) {
               effect = 8;
-              Effekt = "Lamp 100%"; 
               modi = true;
             }else if (header.indexOf("GET /lamp75/on") >= 0) {
               effect = 9;
-              Effekt = "Lamp 75%";
               modi = true;
             }else if (header.indexOf("GET /lamp50/on") >= 0) {
               effect = 10;
-              Effekt = "Lamp 50%";
               modi = true;
             }else if (header.indexOf("GET /lamp25/on") >= 0) {
               effect = 11;
-              Effekt = "Lamp 25%";
               modi = true;
             }else if (header.indexOf("GET /off/on") >= 0) {
               effect = 12;
-              Effekt = "Lamp OFF";
               modi = true;
+            }else if (header.indexOf("GET /resetwifi/on") >= 0) {
+              wm.resetSettings();  
+              ESP.restart();  
+              
             }
 
             // Display the HTML web page
@@ -360,7 +289,7 @@ while (true)
             client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
             client.println(".button { background-color: #195B6A; border: none; color: white; padding: 16px 40px;");
             client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
-            client.println(".button2 {background-color: #77878A;}</style></head>");
+            client.println(".button2 {background-color: #770000; border: none; color: black; padding: 16px 40px; text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}</style></head>");
             
             // Web Page Heading
             client.println("<body><h1>ESP32 Atomic Firelamp Web Server</h1><br>");
@@ -370,25 +299,20 @@ while (true)
             client.print("<h2>Lampeffect: ");
             client.print(Effekt);
             client.println("</h2>");
-            
-                
-            
-              client.println("<p><a href=\"/fire/on\"><button class=\"button\">Fire</button></a></p>");
-              client.println("<p><a href=\"/balls/on\"><button class=\"button\">Balls</button></a></p>");
-              client.println("<p><a href=\"/comet/on\"><button class=\"button\">Comet</button></a></p>");
-              client.println("<p><a href=\"/twinkle/on\"><button class=\"button\">Twinkle</button></a></p>");
-              client.println("<p><a href=\"/marquee/on\"><button class=\"button\">Marquee</button></a></p>");
-              client.println("<p><a href=\"/marquee_m/on\"><button class=\"button\">Marquee mirrored</button></a></p>");
-              client.println("<p><a href=\"/pacifica/on\"><button class=\"button\">Pacifica</button></a></p>");
-              client.println("<p><a href=\"/pride2015/on\"><button class=\"button\">Pride</button></a></p>");
-              client.println("<p><a href=\"/lamp100/on\"><button class=\"button\">Lamp 100%</button></a></p>");
-              client.println("<p><a href=\"/lamp75/on\"><button class=\"button\">Lamp  75%</button></a></p>");
-              client.println("<p><a href=\"/lamp50/on\"><button class=\"button\">Lamp  50%</button></a></p>");
-              client.println("<p><a href=\"/lamp25/on\"><button class=\"button\">Lamp  25%</button></a></p><br>");
-              client.println("<p><a href=\"/off/on\"><button class=\"button\">Lamp OFF</button></a></p>");
-            
-               
-           
+            client.println("<p><a href=\"/fire/on\"><button class=\"button\">Fire</button></a>");
+            client.println("<a href=\"/balls/on\"><button class=\"button\">Balls</button></a></p>");
+            client.println("<p><a href=\"/comet/on\"><button class=\"button\">Comet</button></a>");
+            client.println("<a href=\"/twinkle/on\"><button class=\"button\">Twinkle</button></a></p>");
+            client.println("<p><a href=\"/marquee/on\"><button class=\"button\">Marquee</button></a>");
+            client.println("<a href=\"/marquee_m/on\"><button class=\"button\">Marquee mirrored</button></a></p>");
+            client.println("<p><a href=\"/pacifica/on\"><button class=\"button\">Pacifica</button></a>");
+            client.println("<a href=\"/pride2015/on\"><button class=\"button\">Pride</button></a></p>");
+            client.println("<p><a href=\"/lamp100/on\"><button class=\"button\">Lamp Full</button></a>");
+            client.println("<a href=\"/lamp75/on\"><button class=\"button\">Lamp less bright</button></a></p>");
+            client.println("<p><a href=\"/lamp50/on\"><button class=\"button\">Lamp  medium</button></a>");
+            client.println("<a href=\"/lamp25/on\"><button class=\"button\">Lamp  dimm</button></a></p><br>");
+            client.println("<p><a href=\"/off/on\"><button class=\"button\">Lamp OFF</button></a><br><br><br><br>");
+            client.println("<a href=\"/resetwifi/on\"><button class=\"button2\">Reset Wifi and restart</button></a></p>");
             client.println("</body></html>");
             
             // The HTTP response ends with another blank line
@@ -418,70 +342,16 @@ while (true)
   }
   
  
- if (modi) { //configuration changed, write to flash and display new effect on serial
+ if (modi) { //if mode has changed, write to flash and display new effect on serial
     preferences.begin("Lampe", false);
     preferences.putInt("effect", effect);
     preferences.end();
     Serial.println("Gespeichert. Weiter! ");
-    delay(500);
     modi=false;
-    switch (effect) { //display effect to serial
-   case 1:
-     Serial.println("Effekt: Bälle"); 
-     
 
-      break;
-  
-    case 2:
-    Serial.println("Effekt: Komet");
+    Serial.print("Effect: ");
+    Serial.println(modes[effect]);
     
-      break;  
-    case 3:
-    Serial.println("Effekt: Twinkle");
-   
-      break;
-    
-    case 4:
-    Serial.println("Effekt: Marquee");
-   
-      break;
-    case 5:
-    Serial.println("Effekt: Marquee mirrored");
-   
-      break;
-    case 6:
-    Serial.println("Effekt: Pacifica");
-   
-      break;
-    case 7:
-    Serial.println("Effekt: Pride2015");
-   
-      break;
-    case 11:
-    Serial.println("Effekt: Leselampe 25%");
-   
-      break;  
-    case 10:
-    Serial.println("Effekt: Leselampe 50%");
-   
-      break;  
-    case 9:
-    Serial.println("Effekt: Leselampe 75%");
-   
-      break;  
-     case 8:
-    Serial.println("Effekt: Leselampe 100%");
-   
-      break;  
-    case 12:
-    Serial.println("Effekt: Lampe aus");
-   
-      break;  
-
-
-    default:
-    Serial.println("Effekt: Feuer"); 
-    }; 
  };
  };
 
